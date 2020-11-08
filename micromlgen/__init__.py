2e2cb1b48f52c8da4d9c7aff23750b05959bf85c
Agrimagsrl <web@agrimag.it>
2020-11-08 18:57:59 +0100

    remove convolution wip

--- micromlgen/__init__.py
+++ micromlgen/__init__.py
@@ -2,4 +2,3 @@ import micromlgen.platforms as platforms
 from micromlgen.micromlgen import port
 from micromlgen.utils import port_testset, port_trainset
 from micromlgen.wifiindoorpositioning import port_wifi_indoor_positioning
-from micromlgen.convolution import port_convolution2d
--- micromlgen/convolution.py
+++
@@ -1,8 +0,0 @@
-from micromlgen.utils import jinja
-
-
-def port_convolution2d(input_shape, kernel, suffix=''):
-    """Port optimized convolution 2D"""
-    W, H = input_shape
-    k = int(kernel.shape[0] / 2)
-    return jinja('convolution/convolution2d.jinja', locals())
