0e13130303f91bc1b431b4bb4f3f517c8196b1b3
eloquentarduino <eloquentarduino@gmail.com>
2020-12-27 19:40:02 +0100

    bump dist to 1.1.19

--- setup_template.py
+++ setup_template.py
@@ -7,13 +7,13 @@ package_data = [filename.replace('micromlgen/', '')
 setup(
   name = 'micromlgen',
   packages = ['micromlgen'],
-  version = '1.1.19',
+  version = 'VERSION',
   license='MIT',
   description = 'Generate C code for microcontrollers from Python\'s sklearn classifiers',
   author = 'Simone Salerno',
   author_email = 'eloquentarduino@gmail.com',
   url = 'https://github.com/eloquentarduino/micromlgen',
-  download_url = 'https://github.com/eloquentarduino/micromlgen/blob/master/dist/micromlgen-1.1.19.tar.gz?raw=true',
+  download_url = 'https://github.com/eloquentarduino/micromlgen/blob/master/dist/micromlgen-VERSION.tar.gz?raw=true',
   keywords = [
     'ML',
     'microcontrollers',
